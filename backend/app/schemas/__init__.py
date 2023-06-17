774da7957df54466c6908b0edd0352bd05355f8c
GoukiHino <g.hino@i5net.jp>
2023-06-17 10:17:30 +0900

    docs: namespace

---
+++ backend/app/schemas/__init__.py
@@ -0,0 +1,6 @@
+from .user_base import UserBase
+from .user import UserCreate, UserUpdate, UserResponse
+from .project_base import ProjectBase
+from .project import ProjectCreate, ProjectUpdate, ProjectResponse
+from .task_base import TaskBase
+from .task import TaskCreate, TaskUpdate, TaskResponse
