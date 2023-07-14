a22a2c6bc26084c4daa017a3a6ca99324425ea9e
GoukiHino <g.hino@i5net.jp>
2023-07-14 17:29:24 +0900

    update: add CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.config import settings
 from app.api.v1 import api
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 app.include_router(api.router, prefix=settings.API_PREFIX)
